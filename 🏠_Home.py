050cdecdd2254fd2e6a0d3145600570956ab8171
dhifanrazaqa <dhifanofficial@gmail.com>
2023-11-29 16:55:49 +0700

    add image url

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title('Steganography and Digital Watermarking')
-st.image('images/bg.png')
+st.image('https://res.cloudinary.com/detxefhk2/image/upload/v1701251702/bg_u8aokm.jpg')
 
 st.subheader('Project Mata Kuliah Kriptografi - Informatika UPNVJ', divider='orange')
 st.markdown("Dhi'fan Razaqa (2210511091)")
@@ -11,8 +11,8 @@ st.markdown("Huda Rasyad Wicaksono (2210511098)")
 st.markdown("Dana Christin (2210511115)")
 
 st.subheader('Digital Watermarking', divider='orange', anchor="/google")
-st.image('images\Digital Watermarking.png')
+st.image('https://res.cloudinary.com/detxefhk2/image/upload/v1701251696/Image_Steganography_vdhtps.png')
 st.subheader('Graph Steganography', divider='orange')
-st.image('images\Graph Steganography.png')
+st.image('https://res.cloudinary.com/detxefhk2/image/upload/v1701251691/Graph_Steganography_e9l9vs.png')
 st.subheader('Image Steganography', divider='orange')
-st.image('images\Image Steganography.png')
+st.image('https://res.cloudinary.com/detxefhk2/image/upload/v1701251703/Digital_Watermarking_g8nfzp.png')
